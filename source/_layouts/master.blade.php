456a555ea94dcb96dfa6bb6eb1fed9aeac75e71b
Abraham Kim <gucci@140-182-101-130.ssl-vpn.indiana.edu>
2020-07-04 17:20:58 -0400

    baseUrl on menu links

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -43,7 +43,7 @@
         <header class="flex items-center shadow bg-white border-b h-24 mb-8 py-4" role="banner">
             <div class="container flex justify-between items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center justify-start">
-                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                    <a href="{{ $page->baseUrl }}/" title="{{ $page->siteName }} home" class="inline-flex items-center">
                         <img class="h-8 md:h-10 mr-3 hidden" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
 
                         <h1 class="text-lg md:text-xl text-blue-900 font-thin hover:text-blue-600 my-0 pr-4">
